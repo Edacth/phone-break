abb3251b15dbba30d00b90b7143cf39339567367
Cade Anderson <cade@gamemodeone.com>
2019-11-09 09:31:53 -0800

    Implemented picking up and throwing

---
+++ Assets/Scripts/Grabbable.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grabbable : MonoBehaviour
+{
+    public float throwSpeed = 22.0f;
+    public Vector2 vel;
+    public GameObject myPrefab;
+
+    void Update()
+    {
+        transform.Translate(vel.x * Time.deltaTime * throwSpeed, vel.y * Time.deltaTime * throwSpeed, 0);
+
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Debug.Log(collision.tag);
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            vel = Vector2.zero;
+            Debug.Log("WALL");
+        }
+    }
+}
--- Assets/Scripts/ThrowController.cs
+++ Assets/Scripts/ThrowController.cs
@@ -4,14 +4,65 @@ using UnityEngine;
 
 public class ThrowController : MonoBehaviour
 {
-    //THROWING STUFF GOES HERE
+    Camera cam;
+    Vector3 mouseWorldPoint;
+    Vector2 mousePos;
+    Vector2 aimVector;
+    public GameObject heldObject;
+    public GameObject nearbyObject;
+    
     void Start()
     {
-        
+        cam = Camera.main;
     }
 
     void Update()
     {
-        
+        mousePos = Input.mousePosition;
+
+        mouseWorldPoint = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, cam.nearClipPlane));
+        aimVector = mouseWorldPoint - transform.position;
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (nearbyObject == null) { return; }
+            heldObject = nearbyObject.GetComponent<Grabbable>().myPrefab;
+            GameObject.Destroy(nearbyObject);
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (heldObject == null) { return; }
+            GameObject phoneObj = Instantiate(heldObject, transform.position, Quaternion.identity);
+            Grabbable phone = phoneObj.GetComponent<Grabbable>();
+            phone.vel = aimVector.normalized;
+            heldObject = null;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+        Debug.DrawLine(transform.position, mouseWorldPoint, Color.red);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Grabbable"))
+        {
+            nearbyObject = collision.gameObject;
+            Debug.Log(collision.gameObject + " is nearby");
+        }
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.BeginArea(new Rect(20, 20, 250, 120));
+        GUILayout.Label("Screen pixels: " + cam.pixelWidth + ":" + cam.pixelHeight);
+        //GUILayout.Label("Mouse position: " + mousePos);
+        GUILayout.Label("World position: " + mouseWorldPoint.ToString("F3"));
+        GUILayout.Label("aim Vector: " + aimVector.ToString("F3"));
+        GUILayout.EndArea();
     }
 }
