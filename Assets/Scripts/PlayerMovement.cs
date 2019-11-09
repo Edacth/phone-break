b15ba0edb2704ac775d0617b26ee347ba90a85cb
Cade Anderson <cade@gamemodeone.com>
2019-11-08 22:08:00 -0800

    Initial Scene and movement script

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    float speed = 3.0f;
+    float verticalVelocity;
+    float horizontalVelocity;
+
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        verticalVelocity = Input.GetAxis("Vertical") * speed;
+        horizontalVelocity = Input.GetAxis("Horizontal") * speed;
+
+        transform.Translate(horizontalVelocity * Time.deltaTime, verticalVelocity * Time.deltaTime, 0);
+        //rb.AddForce(new Vector2(horizontalVelocity * Time.deltaTime, verticalVelocity * Time.deltaTime));
+    }
+}
---
+++ Assets/Scripts/ThrowController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowController : MonoBehaviour
+{
+    //THROWING STUFF GOES HERE
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
