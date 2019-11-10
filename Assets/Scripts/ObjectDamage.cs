1e4451575a4a3f95148cfba52a3c545397d8936a
Tyler Peterson <7314tylerpeterson@gmail.com>
2019-11-09 18:02:27 -0800

    Added Object Damage and Router Time

---
+++ Assets/Scripts/ObjectDamage.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectDamage : MonoBehaviour
+{
+    // Used for the tag
+    public string phoneTag = "Grabbable";
+    // Time per seconds you want it to reset the time at
+    public float damagePS = 0.0f;
+    // This will be used for how much damage you want the object to take
+    public float damage = 0.0f;
+    // This is just the timer for the damage
+    float damageTimer = 0.0f;
+    // Boolean to check if it entered and exited the phone
+    bool isColliding = false;
+    void Update()
+    {
+        // Changing the phone damage if its still on the phone with a delay
+        if(isColliding)
+        {
+            damageTimer += Time.deltaTime;
+            if(damageTimer >= damagePS)
+            {
+                damageTimer = 0.0f;
+                PhoneHealth.TakeDamage(damage);
+                Debug.Log(PhoneHealth.phoneHealth);
+            }
+        }
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag(phoneTag))
+        {
+            Debug.Log("A");
+            isColliding = true;
+        }
+    }
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag(phoneTag))
+        {
+            isColliding = false;
+        }
+    }
+}
---
+++ Assets/Scripts/RouterTime.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class RouterTime : MonoBehaviour
+{
+    // Gets a object from the UITimer
+    public UITimer uiTimer;
+    // Used for the tag
+    public string phoneTag = "Grabbable";
+    // Used to get how much it increases the time
+    public float timeIncrease = 0.0f;
+    // warning text
+    public TextMeshProUGUI warningText;
+    // Text opacity when
+    float warningOpacity = 0.0f;
+    void Update()
+    {
+        if (warningOpacity > 0)
+        {
+            warningOpacity -= 1f * Time.deltaTime * 0.5f;
+            warningText.alpha = warningOpacity;
+        }
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag(phoneTag))
+        {
+            uiTimer.time += timeIncrease;
+            warningOpacity = 1;
+            warningText.text = $"{timeIncrease}s added to clock.";
+        }
+    }
+}
