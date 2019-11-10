86c0a3dd5991a01473d0b25f04784fc6c23ec552
TylerRPeterson <54076402+TylerRPeterson@users.noreply.github.com>
2019-11-10 15:57:00 -0800

    Did stuff with the music

---
+++ Assets/Scripts/DontDestory.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestory : MonoBehaviour
+{
+    void Awake()
+    {
+        GameObject[] obj = GameObject.FindGameObjectsWithTag("Music");
+        if(obj.Length > 1)
+            Destroy(this.gameObject);
+            
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
